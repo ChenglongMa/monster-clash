a3314b9f8a571f6c25c461954feb54c632fe5c43
Chenglong Ma <mcl.cn@outlook.com>
2019-05-30 23:40:08 +1000

    Updates

---
+++ precache-manifest.7325e41d72b3043a04b9c2352e79369f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6bc812ce1586b159045a78976f6931",
+    "url": "/monster-clash/index.html"
+  },
+  {
+    "revision": "4be3ace8973d62cde3fb",
+    "url": "/monster-clash/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/monster-clash/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "4be3ace8973d62cde3fb",
+    "url": "/monster-clash/static/js/main.0ae76113.chunk.js"
+  },
+  {
+    "revision": "49cfa0b4c07652709136",
+    "url": "/monster-clash/static/js/runtime~main.eb6bcb7a.js"
+  }
+]);
